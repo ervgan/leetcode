42391147337c69e8daf59c7f9c14c5c430ebe8c0
Ervin Gan <ervin.gan@alumni.upenn.edu>
2023-11-04 20:44:44 +0000

    lowest_common_ancestor

---
+++ tree/common_ancestor.py
@@ -0,0 +1,43 @@
+"""
+https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-search-tree/
+"""
+
+
+class Solution(object):
+    def lowestCommonAncestor(self, root, p, q):
+        """
+        :type root: TreeNode
+        :type p: TreeNode
+        :type q: TreeNode
+        :rtype: TreeNode
+        """
+        while root:
+            if root.val < q.val and root.val < p.val:
+                root = root.right
+            elif root.val > q.val and root.val > p.val:
+                root = root.left
+            else:
+                return root
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def lowestCommonAncestor(self, root, p, q):
+        """
+        :type root: TreeNode
+        :type p: TreeNode
+        :type q: TreeNode
+        :rtype: TreeNode
+        """
+        if not root:
+            return None
+        if root.val < p.val and root.val < q.val:
+            return self.lowestCommonAncestor(root.right, p, q)
+        elif root.val > p.val and root.val > q.val:
+            return self.lowestCommonAncestor(root.left, p, q)
+        return root
