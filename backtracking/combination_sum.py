0b5aa53d5e8293d4012f93d9df00c8612a63def0
Ervin Gan <ervin.gan@alumni.upenn.edu>
2023-11-14 22:50:08 +0000

    comb-sum

---
+++ backtracking/combination_sum.py
@@ -0,0 +1,27 @@
+"""
+https://leetcode.com/problems/combination-sum/
+"""
+
+
+
+class Solution(object):
+    def combinationSum(self, candidates, target):
+        """
+        :type candidates: List[int]
+        :type target: int
+        :rtype: List[List[int]]
+        """
+        ans = []
+        path = []
+
+        def dfs(arr, total, ans, path):
+            if total == target:
+                ans.append(path)
+                return
+            if total > target:
+                return
+            for i in range(len(arr)):
+                dfs(arr[i:], total+arr[i], ans, path + [arr[i]])
+
+        dfs(candidates, 0, ans, path)
+        return ans
