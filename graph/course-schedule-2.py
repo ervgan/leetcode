ea9aea95d5d1463b2fce0116fc1fd7299e130b67
Ervin Gan <ervin.gan@alumni.upenn.edu>
2023-12-02 16:41:59 +0000

    course-schedule-2

---
+++ graph/course-schedule-2.py
@@ -0,0 +1,30 @@
+class Solution(object):
+    def findOrder(self, numCourses, prerequisites):
+        """
+        :type numCourses: int
+        :type prerequisites: List[List[int]]
+        :rtype: List[int]
+        """
+        adj_list = {i : [] for i in range(numCourses)}
+        visited, cycle = set(), set()
+        output = []
+        for course, prereq  in prerequisites:
+            adj_list[course].append(prereq)
+
+        def dfs(course):
+            if course in cycle:
+                return False
+            if course in visited:
+                return True
+            cycle.add(course)
+            for prereq in adj_list[course]:
+                if not dfs(prereq): return False
+            cycle.remove(course)
+            visited.add(course)
+            output.append(course)
+            return True
+
+        for c in range(numCourses):
+            if not dfs(c): return []
+
+        return output
