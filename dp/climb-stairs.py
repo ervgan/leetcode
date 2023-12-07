43b46dbce6488e8742a8918521782ae9031a4999
Ervin Gan <ervin.gan@alumni.upenn.edu>
2023-12-07 22:28:52 +0000

    climb-stairs

---
+++ dp/climb-stairs.py
@@ -0,0 +1,18 @@
+"""
+https://leetcode.com/problems/climbing-stairs/
+"""
+
+#O(n)
+class Solution(object):
+    def climbStairs(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        one, two = 1, 1
+
+        for i in range(n-1):
+          temp = one
+          one = one + two
+          two = temp
+        return one
